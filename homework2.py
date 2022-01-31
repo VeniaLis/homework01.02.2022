540bf3a93489076afd6560a03163531d957eff46
Venialis <nfs41738@mail.ru>
2022-01-31 23:16:25 +0300

    answer the fourth task

---
+++ homework2.py
@@ -0,0 +1,13 @@
+'''def brain_function(a, b = 0):
+
+    if a >= 10:
+        return 'Да'
+    else:
+        return 'Нет'
+
+
+number1 = int(input('Введите число: '))
+number2 = int(input('Введите второе число: '))
+my_answer = brain_function(number1, number2)
+
+print(my_answer)'''
